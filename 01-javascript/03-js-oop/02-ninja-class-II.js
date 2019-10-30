2a904c4c261d10ffb4ebcb3fe7b38dbf499437e8
tomerltn <tramble.tomerlin@gmail.com>
2019-10-30 17:20:42 -0400

    updated ninjaII again

--- 01-javascript/03-js-oop/02-ninja-class-II.js
+++ 01-javascript/03-js-oop/02-ninja-class-II.js
@@ -2,7 +2,7 @@ function Ninja(name){
     this.name = name;
     this.health = 100;
     this.speed = 3;
-    this.stregth = 3;
+    this.strength = 3;
 }
 Ninja.prototype.sayName = function() {
     console.log("Hey fellow ninjas, my name is" + " " + this.name );
@@ -11,23 +11,30 @@ Ninja.prototype.sayName = function() {
 Ninja.prototype.showStats = function() {
     console.log("Name:"+ this.name,"Health:" + this.health, 'Speed:' + this.speed, 'Strength:' + this.stregth );
 };
-Ninja.prototype.punch = function() {
+Ninja.prototype.punch = function(ninja) {
     this.health -= 5;
     console.log(ninja.name + " was punched by " + this.name + " and lost 5 Health!");
     return this;
 
 }
-Ninja.prototype.drinkSake = function() {
+Ninja.prototype.drinkSake = function(ninja) {
     this.health += 10;
     return this;
 }
-this.kick = function(ninja) {
-    const damage = strength * 5;
+Ninja.prototype.kick = function(ninja) {
+    const damage = this.strength * 5;
     ninja.health -= damage;
-    console.log(ninja.name + "was punched by" + this.name + "and lost 5 health!");
+    console.log(ninja.name + "was kicked by" + this.name + "and lost 5 health!");
     return this;
 }
 
+
+const tramble = new Ninja('Tramble');
+
+const blueNinja = new Ninja('Goemon');
+
+const redNinja = new Ninja('Bill Gates');
+console.log(this);
 redNinja.punch(blueNinja);
 blueNinja.kick(blueNinja);
 
