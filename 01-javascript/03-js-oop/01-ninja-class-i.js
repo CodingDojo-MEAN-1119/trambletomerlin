6a7bd55574660e30e6c043f74163c949ccc514b0
tomerltn <tramble.tomerlin@gmail.com>
2019-10-30 15:57:49 -0400

    added ninja 1 OOP

---
+++ 01-javascript/03-js-oop/01-ninja-class-i.js
@@ -0,0 +1,19 @@
+function Ninja(name){
+    this.name = name;
+    this.health = 100;
+    this.speed = 3;
+    this.stregth = 3;
+}
+Ninja.prototype.sayName = function() {
+    console.log("Hey fellow ninjas, my name is" + " " + this.name );
+    return this;
+};
+Ninja.prototype.showStats = function() {
+    console.log("Name:"+ this.name,"Health:" + this.health, 'Speed:' + this.speed, 'Strength:' + this.stregth );
+};
+
+var tramble = new Ninja('Tramble');
+
+tramble.sayName();
+
+tramble.showStats();
