f9e4e0bb64c9b1e5afa42576e673abe22e53d10e
tomerltn <tramble.tomerlin@gmail.com>
2019-11-01 09:17:26 -0400

    cars and cats

---
+++ 02-node/01-node/02-cars-and-cats/cars_and_cats/apps.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer((request, response) => {
+    if(request.url === '/') {
+        fs.readFile('cars.html', 'utf8', (errors, contents) => {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    if(request.url === '/cars') {
+        fs.readFile('cars.html', 'utf8', (errors, contents) => {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    if (request.url === "/cats") {
+         fs.readFile('cats.html', 'utf8', (errors, contents) => {
+             response.writeHead(200, {'Content-type': 'text/html'});
+             response.write(contents); 
+             response.end();
+         });
+    }
+    if (request.url === "/cars/new") {
+        fs.readFile('cars_new.html', 'utf8', (errors, contents) => {
+            response.writeHead(200, {'Content-type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+   }
+    else {
+        response.end('File not found!!!');
+    }
+});
+server.listen(7707);
+console.log("listening on port 7707");
