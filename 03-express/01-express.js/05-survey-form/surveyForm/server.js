a3e9359571656ee7582369db9ccf697fe2f91cb1
tomerltn <tramble.tomerlin@gmail.com>
2019-11-03 11:07:18 -0500

    finished survey

--- 03-express/01-express.js/05-survey-form/surveyForm/server.js
+++ 03-express/01-express.js/05-survey-form/surveyForm/server.js
@@ -3,15 +3,16 @@ const app = express();
 app.use(express.static(__dirname + "/static"));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+
 app.use(express.urlencoded({extended: true}));
 
 
 app.get('/', (req, res) => {
    res.render('index', {title: "my root route"});
 });
-app.post('/form',(req,res) => {
-    console.log(req.body)
-    render.redirect('/result');
+app.post('/result',(req,res) => {
+    results = req.body
+    res.render('result', {results: results});
 });
 
 app.listen(8000, () => console.log("listening on port 8000"));
